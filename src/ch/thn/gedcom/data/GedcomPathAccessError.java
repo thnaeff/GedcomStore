aa290c335ecbd0a84a82de9138de2e7f91738495
thnaeff <thnaeff@bluewin.ch>
2014-01-17 23:10:26 -0500

    Updated License (Apache v2) and author information

--- src/ch/thn/gedcom/data/GedcomPathAccessError.java
+++ src/ch/thn/gedcom/data/GedcomPathAccessError.java
@@ -1,4 +1,17 @@
 /**
+ *    Copyright 2013 Thomas Naeff (github.com/thnaeff)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  */
 package ch.thn.gedcom.data;
@@ -8,7 +21,7 @@ package ch.thn.gedcom.data;
 * an error description and the access path is the path which caused the error
 * (retrieve the path with {@link #getAccessPath()}).
 *
-* @author thomas
+* @author Thomas Naeff (github.com/thnaeff)
 *
 */
 public class GedcomPathAccessError extends GedcomError {
--- src/ch/thn/gedcom/data/GedcomTree.java
+++ src/ch/thn/gedcom/data/GedcomTree.java
@@ -1,4 +1,17 @@
 /**
+ *    Copyright 2013 Thomas Naeff (github.com/thnaeff)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  */
 package ch.thn.gedcom.data;
@@ -6,7 +19,7 @@ package ch.thn.gedcom.data;
 import ch.thn.gedcom.store.GedcomStoreStructure;
 
 /**
- * @author thomas
+ * @author Thomas Naeff (github.com/thnaeff)
  *
  */
 public class GedcomTree extends GedcomNode {
